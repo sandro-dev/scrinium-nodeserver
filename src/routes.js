00905bc9f2dc3cef20be3a7b341feb0d149b4f07
Sandro Santos <sandro.dev@gmail.com>
2020-01-30 17:17:04 -0300

    initial structure to node server

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+  return res.json({message: 'The server is running'})
+})
+
+export default routes;
